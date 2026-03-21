c10758adaa1b8ac4ae62fef2673b477fa310c651
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-03-21 22:54:27 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,12 +4,27 @@ from .models import *
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
+import io
+from rest_framework.parsers import JSONParser
+
 
 
 # Create your views here.
  
 
 def student(req):
+    if req.method=="POST":
+        data=req.body
+        stream= io.BytesIO(data)
+        p_data = JSONParser().parse(stream)
+        serializer-Stu_serializer(data=p_data)
+        if serializer.is_valid():
+            serializer.save()
+            return HttpResponse({"msg":"object created"},content_type='application/json')
+        else:
+            data=JSONRenderer().render(serializer.errors)
+            return HttpResponse(data,content_type='application/json')
+
     data=Student.objects.all()
     serializer=Stu_serializer(data,many=True)
     print(serializer)
@@ -25,4 +40,8 @@ def student(req):
     json = JSONRenderer().render(serializer.data)
     print(json)
     return HttpResponse(json,content_type='application/json')
+
+
+
+#Deserializer
 
